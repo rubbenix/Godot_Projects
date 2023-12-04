0959cb29b2343c82fcf28b4cef20bb6552400a50
rubbenix <rubenix02@gmail.com>
2023-12-04 14:23:44 +0100

    New Project

--- 0_Starting/main.gd
+++ 0_Starting/main.gd
@@ -7,3 +7,4 @@ func _on_quit_button_pressed():
 
 func _on_play_button_pressed():
 	get_tree().change_scene_to_file("res://world.tscn")
+	
---
+++ 1_Godot_Hill_Climb/Scripts/Player.gd
@@ -0,0 +1,22 @@
+extends RigidBody2D
+
+var wheels = []
+var speed = 50000
+var max_speed_right = 40
+var max_speed_left = 10
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	wheels = get_tree().get_nodes_in_group("wheel")
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta):
+	if Input.is_action_pressed("ui_right"):
+		for wheel in wheels:
+			if wheel.angular_velocity < max_speed_right:
+				wheel.apply_torque_impulse(speed * delta * 60)
+	if Input.is_action_pressed("ui_left"):
+		for wheel in wheels:
+			if wheel.angular_velocity > -max_speed_left:
+				wheel.apply_torque_impulse(-speed * delta * 60)
+#func _restart():
+#	if( )
